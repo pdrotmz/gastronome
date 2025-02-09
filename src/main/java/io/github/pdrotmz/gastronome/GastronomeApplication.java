5d82d4859d3d1a5b377c8d18ae514495ea2dd90f
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-09 14:21:32 -0300

    initial commit

---
+++ src/main/java/io/github/pdrotmz/gastronome/GastronomeApplication.java
@@ -0,0 +1,13 @@
+package io.github.pdrotmz.gastronome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastronomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastronomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pdrotmz/gastronome/GastronomeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pdrotmz.gastronome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastronomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
