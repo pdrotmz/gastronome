77ce6b90b7fd893a9a680647bd64645b6d0a202e
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-11 00:32:02 -0300

    fix: fixes delete customer by id error

--- src/main/java/io/github/pdrotmz/gastronome/customer/repository/CustomerRepository.java
+++ src/main/java/io/github/pdrotmz/gastronome/customer/repository/CustomerRepository.java
@@ -2,8 +2,10 @@ package io.github.pdrotmz.gastronome.customer.repository;
 
 import io.github.pdrotmz.gastronome.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,6 +15,8 @@ public interface CustomerRepository extends JpaRepository<Customer, String> {
     @Query(value = "SELECT * FROM tb_customer c WHERE c.email = ?1", nativeQuery = true)
     Optional<Customer> findCustomerByEmail(String email);
 
+    @Modifying
+    @Transactional
     @Query(value = "DELETE FROM tb_customer c WHERE c.id = ?1", nativeQuery = true)
     void deleteCustomerById(String id);
 }
