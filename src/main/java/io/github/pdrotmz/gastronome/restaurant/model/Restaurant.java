2e3da27e5a4b87b37bbbf0bb29c6ecc26fc0b3e0
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-11 00:32:21 -0300

    feat: add restaurant model

---
+++ src/main/java/io/github/pdrotmz/gastronome/restaurant/model/Restaurant.java
@@ -0,0 +1,44 @@
+package io.github.pdrotmz.gastronome.restaurant.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "restaurant")
+@Table(name = "tb_restaurant")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "address", nullable = false, length = 255)
+    private String address;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
